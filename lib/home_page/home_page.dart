d4edd6b2ba10a89e65c78ceab008cbf8715bf177
ChrisK00 <christian.khalil2000@gmail.com>
2024-08-29 17:53:44 +0200

    fix search

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -62,7 +62,10 @@ class _HomePageState extends State<HomePage> {
               ? const Icon(Icons.search)
               : IconButton(
                   icon: const Icon(Icons.clear),
-                  onPressed: () => clearSearch(provider))),
+                  onPressed: () {
+                    clearSearch(provider);
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  })),
           contentPadding: const EdgeInsets.only(left: 10, top: 10)),
       onChanged: (value) => onSearch(value, provider),
       onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
@@ -72,7 +75,6 @@ class _HomePageState extends State<HomePage> {
   Future clearSearch(NotesProvider provider) async {
     final loadedNotes = await HomePageRepository.loadNotes();
     provider.init(loadedNotes);
-    FocusManager.instance.primaryFocus?.unfocus();
     setState(() => _searchTextController.text = '');
   }
 
